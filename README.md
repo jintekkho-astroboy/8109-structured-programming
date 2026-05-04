dba20b74e5b6c66264cc6f1a8c2d9cac94be8e88
Jin Tek <jintek.kho@gmail.com>
2026-05-04 05:56:13 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 8109-structured-programming
+
+This is an example of using structued proramming to create a todo list
---
+++ index.js
@@ -0,0 +1,36 @@
+//1. define the state and create some mock data 
+let tasks = [
+    {
+        "id": 1,
+        "name":"Wash the car",
+        "dateDue":"2026-05-05",
+        "urgency":3
+    },
+    {
+        "id": 2,
+        "name":"Clean the room",
+        "dateDue":"2026-05-31",
+        "urgency":1
+    },
+    {
+        "id": 3,
+        "name":"Buy cat food",
+        "dateDue":"2026-05-25",
+        "urgency":5
+    }
+]
+
+// tasks will be an array of objects
+// and each objects must have the following id, name, dataDue and urgency
+function displayTasks(tasks) {
+    let i = 0;
+    // tasks.length will give us how many items are in the array
+    while (i<tasks.length) {
+        let currentTask = tasks[i];
+        console.log(`${currentTask.id}. ${currentTask.name} is due on ${currentTask.dateDue} with urgency ${currentTask.urgency}`);
+        // example output
+        //"1. Wash the care is due on 2026-05-05 with urgency 3"
+        i=i+1;
+    }
+}
+displayTasks(tasks);
